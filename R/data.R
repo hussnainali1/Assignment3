5417070b731533491d2d82e7582f2e6639f7c9e3
Hussnain Ali <hussnainkhilgi1@gmail.com>
2022-09-12 19:27:22 +0200

    changes data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Title
+#'
+#' Description
+#'
+#' @format A data frame with nodes and weight:
+#' \describe{
+#' \item{v1}{this is the First Nodes v1}
+#' \item{v2}{this is the 2nd Nodes v2}
+#' \item{w}{this is the weight between Nodes}
+#' }
+#'
+'wiki_graph'
+
+
+
